b85f9538ad5437def8fbcc1c6da0453a026e83c8
Emir Cognigni <emir.cognigni@aton.com>
2023-01-19 21:02:52 +0100

    Initial Commit

---
+++ Studyamo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Studyamo
+//
+//  Created by Emir Fabio Cognigni on 19/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StudyamoUITests/StudyamoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StudyamoUITestsLaunchTests.swift
+//  StudyamoUITests
+//
+//  Created by Emir Fabio Cognigni on 19/01/23.
+//
+
+import XCTest
+
+class StudyamoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
