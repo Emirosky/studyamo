c5b63e54f2498efe3badca713617286f0a9f5ac4
Emir Cognigni <emir.cognigni@aton.com>
2023-01-19 21:42:36 +0100

    First screen

--- Studyamo/ViewController.swift
+++ Studyamo/ViewController.swift
@@ -11,7 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let label = UILabel()
+        label.text = "Ciao"
+        
+        view.addSubview(label)
+        
+        var constraints = [
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
     }
 
 
